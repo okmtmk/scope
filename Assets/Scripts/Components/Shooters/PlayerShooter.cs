eae57835da4654861d26f525365013e3d91f8f4b
okmtmk <c0118060b4@edu.teu.ac.jp>
2019-10-29 16:06:22 +0900

    [add]C#ファイルの配置例

---
+++ Assets/Scripts/Components/Shooters/PlayerShooter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Scripts.Components.Shooters
+{
+    public class PlayerShooter : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
