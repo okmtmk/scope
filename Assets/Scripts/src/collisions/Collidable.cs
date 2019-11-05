1423b8f8ca2408e8abb3813c7a514ad82446471c
okmtmk <c0118060b4@edu.teu.ac.jp>
2019-11-05 14:21:03 +0900

    [add]初めてあたったときと、当たり判定から抜けたときのハンドラ追加

--- Assets/Scripts/src/collisions/Collidable.cs
+++ Assets/Scripts/src/collisions/Collidable.cs
@@ -10,6 +10,19 @@ namespace src.collisions
         float Width { get; }
         float Height { get; }
 
+        /**
+         * あたっている間の処理
+         */
         void OnCollide(ICollidable collidable);
+
+        /**
+         * 初めてあたったときの処理
+         */
+        void OnEnterCollider(ICollidable collidable);
+
+        /**
+         * 当たり判定を抜けたときの処理
+         */
+        void OnExitCollider(ICollidable collidable);
     }
 }
