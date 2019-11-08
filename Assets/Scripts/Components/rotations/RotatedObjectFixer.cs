b866e66155124b4b26b7f22f942750f18e854b14
okmtmk <c0118060b4@edu.teu.ac.jp>
2019-11-08 14:28:24 +0900

    [add]カメラ回転さすやつと、画面上の位置を保つやつ

--- Assets/Scripts/Components/rotations/RotatedObjectFixer.cs
+++ Assets/Scripts/Components/rotations/RotatedObjectFixer.cs
@@ -1,9 +1,47 @@
+using System;
 using UnityEngine;
 
 namespace Components.rotations
 {
     public class RotatedObjectFixer : MonoBehaviour
     {
-        
+        [SerializeField] private MouseInputRotator rotator;
+
+        private float X
+        {
+            get => gameObject.transform.position.x;
+            set
+            {
+                var position = gameObject.transform.position;
+                position.Set(
+                    value,
+                    position.y,
+                    position.z
+                );
+            }
+        }
+
+        private float Y
+        {
+            get => gameObject.transform.position.y;
+            set
+            {
+                var position = gameObject.transform.position;
+                position.Set(
+                    position.x,
+                    value,
+                    position.z
+                );
+            }
+        }
+
+        private void Update()
+        {
+            var distance = Math.Sqrt(X * X + Y * Y);
+            var radian = Math.Atan2(Y, X) + rotator.RadianZ;
+
+            X = (float) (Math.Cos(radian) * distance);
+            Y = (float) (Math.Sin(radian) * distance);
+        }
     }
 }
