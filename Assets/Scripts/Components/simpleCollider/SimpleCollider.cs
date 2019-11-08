3f71d18ecab8d0567840ac590b185593fa48bd46
okmtmk <c0118060b4@edu.teu.ac.jp>
2019-11-08 12:02:51 +0900

    [add]Colliderの自前実装

--- Assets/Scripts/Components/simpleCollider/SimpleCollider.cs
+++ Assets/Scripts/Components/simpleCollider/SimpleCollider.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using src.collisions;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,15 +7,15 @@ namespace Components.simpleCollider
 {
     public class SimpleCollider : MonoBehaviour
     {
-        [SerializeField] private Sprite sprite;
-        [SerializeField] private UnityEvent<GameObject> onColliderEnter;
-        [SerializeField] private UnityEvent<GameObject> onColliding;
-        [SerializeField] private UnityEvent<GameObject> onColliderExit;
+        [SerializeField] private SpriteRenderer sprite;
+        [SerializeField] private UnityEvent<SimpleCollider> onColliderEnter;
+        [SerializeField] private UnityEvent<SimpleCollider> onColliding;
+        [SerializeField] private UnityEvent<SimpleCollider> onColliderExit;
 
-        [NonSerialized] private static List<SimpleCollider> _colliders
+        [NonSerialized] private static readonly List<SimpleCollider> Colliders
             = new List<SimpleCollider>();
 
-        [NonSerialized] private static Dictionary<SimpleCollider, SimpleCollider> _collideds
+        [NonSerialized] private static readonly Dictionary<SimpleCollider, SimpleCollider> Collideds
             = new Dictionary<SimpleCollider, SimpleCollider>();
 
         private float X => gameObject.transform.position.x;
@@ -27,14 +26,71 @@ namespace Components.simpleCollider
 
         private void Start()
         {
-            _colliders.Add(this);
+            Colliders.Add(this);
         }
 
         private void Update()
         {
+            var listA = new List<SimpleCollider>(Colliders);
             var decideds = new List<SimpleCollider>();
-            
-            
+
+            listA.ForEach(a =>
+            {
+                if (decideds.Contains(a))
+                {
+                    return;
+                }
+
+                var listB = new List<SimpleCollider>(listA);
+                listB.Remove(a);
+
+                listB.ForEach(b =>
+                {
+                    if (IsColliding(a, b))
+                    {
+                        HandleColliding(a, b);
+                        decideds.Add(b);
+                    }
+                    else
+                    {
+                        HandleColliderExit(a);
+                    }
+                });
+            });
+        }
+
+        private static bool IsColliding(SimpleCollider a, SimpleCollider b)
+        {
+            return Math.Abs(a.X - b.X) < a.Width / 2 + b.Width / 2 &&
+                   Math.Abs(a.Y - b.Y) < a.Height / 2 + b.Height / 2;
+        }
+
+        private static void HandleColliding(SimpleCollider a, SimpleCollider b)
+        {
+            if (!Collideds.ContainsKey(a))
+            {
+                a.onColliderEnter.Invoke(b);
+                Collideds.Add(a, b);
+            }
+
+            if (!Collideds.ContainsKey(b))
+            {
+                b.onColliderEnter.Invoke(a);
+                Collideds.Add(b, a);
+            }
+
+            a.onColliding.Invoke(b);
+            b.onColliding.Invoke(a);
+        }
+
+        private static void HandleColliderExit(SimpleCollider obj)
+        {
+            if (Collideds.ContainsKey(obj))
+            {
+                var target = Collideds[obj];
+                obj.onColliderExit.Invoke(target);
+                Collideds.Remove(obj);
+            }
         }
     }
 }
