284734fc8dbf17c0ff5092fd8341e95c7fd389c2
okmtmk <c0118060b4@edu.teu.ac.jp>
2019-11-08 12:09:05 +0900

    [add]キー入力でオブジェクトを移動させるComponent

--- Assets/Scripts/Components/positions/KeyInputMover.cs
+++ Assets/Scripts/Components/positions/KeyInputMover.cs
@@ -12,6 +12,37 @@ namespace Components.positions
 
         [SerializeField] private float moveSpeed = 15;
 
+        private float X
+        {
+            get => gameObject.transform.position.x;
+            set
+            {
+                var position = gameObject.transform.position;
+                position.Set(
+                    value,
+                    position.y,
+                    position.z
+                );
+            }
+        }
+
+        private float Y
+        {
+            get => gameObject.transform.position.y;
+            set
+            {
+                var position = gameObject.transform.position;
+                position.Set(
+                    position.x,
+                    value,
+                    position.z
+                );
+            }
+        }
+
+        private float RadianZ => gameObject.transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+        private long ElapsedSecond => _stopwatch.ElapsedMilliseconds / 1000;
+
         private void Start()
         {
             _position2 = new MovablePosition2(gameObject.transform.position, 15);
@@ -23,22 +54,24 @@ namespace Components.positions
             _position2.Sync(gameObject.transform.position);
             var direction = MovingKey.GetMovingDirection();
 
-            if (!(direction.x == 0 && direction.y == 0))
+            if (Math.Abs(direction.x) > 0 || Math.Abs(direction.y) > 0)
             {
-                float radius = (float) Math.Atan2(direction.y, direction.x) +
-                               gameObject.transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
-
-                _position2.Move(radius, moveSpeed, _stopwatch.ElapsedMilliseconds);
-                _position2.FitMovableArea();
+                var radian = (float) Math.Atan2(direction.y, direction.x) + RadianZ;
+                Move(radian);
+                
                 _stopwatch.Restart();
-
-                gameObject.transform.position = new Vector3(_position2.X, _position2.Y, 0);
             }
             else
             {
                 _stopwatch.Stop();
                 _stopwatch.Reset();
             }
+        }
+
+        private void Move(float radian)
+        {
+            X += (float) Math.Cos(radian) * moveSpeed * ElapsedSecond;
+            Y += (float) Math.Sin(radian) * moveSpeed * ElapsedSecond;
         }
     }
 }
