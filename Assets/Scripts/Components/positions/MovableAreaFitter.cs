5a0e650f783b2f13151938ad1dffb6046562c004
okmtmk <c0118060b4@edu.teu.ac.jp>
2019-11-08 14:57:48 +0900

    [add]行動範囲制限スクリプト

--- Assets/Scripts/Components/positions/MovableAreaFitter.cs
+++ Assets/Scripts/Components/positions/MovableAreaFitter.cs
@@ -1,9 +1,32 @@
+using System;
 using UnityEngine;
 
 namespace Components.positions
 {
     public class MovableAreaFitter : MonoBehaviour
     {
-        
+        [SerializeField] private int movableDistance = 15;
+
+        private float X
+        {
+            get => gameObject.transform.position.x;
+            set => gameObject.transform.position = new Vector2(value, gameObject.transform.position.y);
+        }
+
+        private float Y
+        {
+            get => gameObject.transform.position.y;
+            set => gameObject.transform.position = new Vector2(gameObject.transform.position.x, value);
+        }
+
+        private void Update()
+        {
+            if (Math.Sqrt(X * X + Y * Y) > 15)
+            {
+                var radian = Math.Atan2(Y, X);
+                X = (float) (Math.Cos(radian) * movableDistance);
+                Y = (float) (Math.Sin(radian) * movableDistance);
+            }
+        }
     }
 }
