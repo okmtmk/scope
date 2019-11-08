196c067709e1102b524afcc775172b2c1e029425
okmtmk <c0118060b4@edu.teu.ac.jp>
2019-11-08 14:28:24 +0900

    [add]カメラ回転さすやつと、画面上の位置を保つやつ

--- Assets/Scripts/Components/rotations/MouseInputRotator.cs
+++ Assets/Scripts/Components/rotations/MouseInputRotator.cs
@@ -1,9 +1,27 @@
+using System;
 using UnityEngine;
 
 namespace Components.rotations
 {
     public class MouseInputRotator : MonoBehaviour
     {
-        
+        [SerializeField] private int rotateSpeed = 5;
+
+        [NonSerialized] private float _mouseBasePositionX;
+
+        public float RadianZ => gameObject.transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
+        private float Angle => -(Input.mousePosition.x - _mouseBasePositionX) / 10 * rotateSpeed;
+
+
+        private void Start()
+        {
+            _mouseBasePositionX = Input.mousePosition.x;
+        }
+
+        private void Update()
+        {
+            gameObject.transform.rotation =
+                Quaternion.Euler(0f, 0f, Angle);
+        }
     }
 }
