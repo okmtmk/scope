db2009f0281c60071c1deae08e16599054ee5c87
--local <--local>
2019-11-09 20:36:31 +0900

    [update]マウスカーソルを非表示に

--- Assets/Scripts/Components/rotations/RotationZLinker.cs
+++ Assets/Scripts/Components/rotations/RotationZLinker.cs
@@ -6,10 +6,18 @@ namespace Components.rotations
     public class RotationZLinker : MouseInputRotator
     {
         [SerializeField] private MouseInputRotator rotator;
-        
+        [SerializeField] private bool isFix;
+
         protected override void Update()
         {
-            RotationZ = rotator.Angle;
+            if (isFix)
+            {
+                RotationZ = 0;
+            }
+            else
+            {
+                RotationZ += rotator.Angle;
+            }
         }
     }
 }
