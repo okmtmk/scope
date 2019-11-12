8775e6e3b6479e509377c1becc785345527ad682
--local <--local>
2019-11-12 22:08:06 +0900

    [add]レベルデザイン途中

--- Assets/Scripts/Components/levels/Level1.cs
+++ Assets/Scripts/Components/levels/Level1.cs
@@ -12,185 +12,151 @@ namespace Components.levels
         [SerializeField] private Enemy curveRightEnemy;
         [SerializeField] private Enemy curveLeftEnemy;
 
-        public override void Register(Dictionary<long, Action<LevelPlayer>> spawnEvent)
+        public override void Register(Dictionary<long, Action<LevelPlayer>> events)
         {
-            spawnEvent.Add(1000, player =>
-            {
-                const float num = 0f;
-                const float y = 30f;
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num - 1.5f, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num + 1.5f, y, 0);
-            });
+            SpawnGoStraightAndStopCube(events, 0, 20, 500);
+            SpawnGoStraightAndStopCube(events, 6, 20, 2500);
+            SpawnGoStraightAndStopCube(events, -6, 20, 4500);
+            
+            SpawnCurveRightCube(events, 16, 16, 6000, -45);
+            SpawnCurveLeftCube(events, -16, 16, 8000, 45);
+            
+            SpawnGoStraightDia(events, 0, 20, 9500);
+            SpawnGoStraightDia(events, 6, 20, 11500);
+            SpawnGoStraightDia(events, -6, 20, 13500);
+            
+            SpawnGoStraightDia(events, 16, 16, 16000,-30);
+            SpawnGoStraightDia(events, 16, 19, 17000,-30);
+            
+            SpawnGoStraightDia(events, -16, 16, 20000,30);
+            SpawnGoStraightDia(events, -16, 19, 21000,30);
+        }
 
-            spawnEvent.Add(1100, player =>
+        private void SpawnRow(
+            Dictionary<long, Action<LevelPlayer>> spawnEvent,
+            Enemy enemy,
+            float baseX,
+            float baseY,
+            long baseMilliSecond,
+            float rotationZ = 0)
+        {
+            spawnEvent.Add(baseMilliSecond, player =>
             {
-                const float num = 0f;
-                const float y = 31.5f;
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num - 1.5f, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num + 1.5f, y, 0);
+                var x = baseX;
+                var y = baseY;
+                player.SpawnEnemy(enemy, x - 1.5f, y, rotationZ);
+                player.SpawnEnemy(enemy, x, y, rotationZ);
+                player.SpawnEnemy(enemy, x + 1.5f, y, rotationZ);
             });
+        }
 
-            spawnEvent.Add(1200, player =>
-            {
-                const float num = 0f;
-                const float y = 33f;
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num - 1.5f, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num + 1.5f, y, 0);
-            });
+        private void SpawnGoStraightAndStopCube(
+            Dictionary<long, Action<LevelPlayer>> spawnEvent,
+            float baseX,
+            float baseY,
+            long baseMilliSecond,
+            float rotationZ = 0)
+        {
+            SpawnRow(spawnEvent, goStraightAndStopEnemy, baseX, baseY, baseMilliSecond, rotationZ);
+            SpawnRow(spawnEvent, goStraightAndStopEnemy, baseX, baseY + 1.5f, baseMilliSecond + 100, rotationZ);
+            SpawnRow(spawnEvent, goStraightAndStopEnemy, baseX, baseY + 3, baseMilliSecond + 200, rotationZ);
+        }
 
+        private void SpawnCurveRightCube(
+            Dictionary<long, Action<LevelPlayer>> events,
+            float baseX,
+            float baseY,
+            long baseMilliSecond,
+            float rotationZ = 0)
+        {
+            SpawnRow(events, curveRightEnemy, baseX, baseY, baseMilliSecond, rotationZ);
+            SpawnRow(events, curveRightEnemy, baseX, baseY + 1.5f, baseMilliSecond + 100, rotationZ);
+            SpawnRow(events, curveRightEnemy, baseX, baseY + 3, baseMilliSecond + 200, rotationZ);
+        }
 
-            spawnEvent.Add(3200, player =>
-            {
-                const float num = 3f;
-                const float y = 30f;
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num - 1.5f, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num + 1.5f, y, 0);
-            });
+        private void SpawnCurveLeftCube(
+            Dictionary<long, Action<LevelPlayer>> events,
+            float baseX,
+            float baseY,
+            long baseMilliSecond,
+            float rotationZ = 0)
+        {
+            SpawnRow(events, curveLeftEnemy, baseX, baseY, baseMilliSecond, rotationZ);
+            SpawnRow(events, curveLeftEnemy, baseX, baseY + 1.5f, baseMilliSecond + 100, rotationZ);
+            SpawnRow(events, curveLeftEnemy, baseX, baseY + 3, baseMilliSecond + 200, rotationZ);
+        }
 
-            spawnEvent.Add(3300, player =>
+        private void SpawnGoStraightDia(
+            Dictionary<long, Action<LevelPlayer>> events,
+            float baseX,
+            float baseY,
+            long baseMilliSecond,
+            float rotationZ = 0)
+        {
+            events.Add(baseMilliSecond, player =>
             {
-                const float num = 3f;
-                const float y = 31.5f;
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num - 1.5f, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num + 1.5f, y, 0);
-            });
+                player.SpawnEnemy(
+                    goStraightEnemy,
+                    baseX,
+                    baseY,
+                    rotationZ
+                );
 
-            spawnEvent.Add(3400, player =>
-            {
-                const float num = 3f;
-                const float y = 33f;
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num - 1.5f, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num + 1.5f, y, 0);
-            });
+                player.SpawnEnemy(
+                    goStraightEnemy,
+                    baseX + 1.5f,
+                    baseY + 1.5f,
+                    rotationZ
+                );
 
+                player.SpawnEnemy(
+                    goStraightEnemy,
+                    baseX - 1.5f,
+                    baseY + 1.5f,
+                    rotationZ
+                );
 
-            spawnEvent.Add(5400, player =>
-            {
-                const float num = -3f;
-                const float y = 30f;
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num - 1.5f, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num + 1.5f, y, 0);
-            });
+                player.SpawnEnemy(
+                    goStraightEnemy,
+                    baseX,
+                    baseY + 3,
+                    rotationZ
+                );
 
-            spawnEvent.Add(5500, player =>
-            {
-                const float num = -3f;
-                const float y = 31.5f;
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num - 1.5f, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num + 1.5f, y, 0);
-            });
+                player.SpawnEnemy(
+                    goStraightEnemy,
+                    baseX + 3f,
+                    baseY + 3f,
+                    rotationZ
+                );
 
-            spawnEvent.Add(5600, player =>
-            {
-                const float num = -3f;
-                const float y = 33f;
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num - 1.5f, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num, y, 0);
-                player.SpawnEnemy(goStraightAndStopEnemy,
-                    num + 1.5f, y, 0);
-            });
+                player.SpawnEnemy(
+                    goStraightEnemy,
+                    baseX - 3f,
+                    baseY + 3f,
+                    rotationZ
+                );
 
-            spawnEvent.Add(7600, player =>
-            {
-                const float num = 15f;
-                const float y = 25f;
-                player.SpawnEnemy(curveRightEnemy,
-                    num - 1.5f, y, -30);
-                player.SpawnEnemy(curveRightEnemy,
-                    num, y, -30);
-                player.SpawnEnemy(curveRightEnemy,
-                    num + 1.5f, y, -30);
-            });
-            spawnEvent.Add(7700, player =>
-            {
-                const float num = 15f;
-                const float y = 26.5f;
-                player.SpawnEnemy(curveRightEnemy,
-                    num - 1.5f, y, -30);
-                player.SpawnEnemy(curveRightEnemy,
-                    num, y, -30);
-                player.SpawnEnemy(curveRightEnemy,
-                    num + 1.5f, y, -30);
-            });
-            spawnEvent.Add(7800, player =>
-            {
-                const float num = 15f;
-                const float y = 28f;
-                player.SpawnEnemy(curveRightEnemy,
-                    num - 1.5f, y, -30);
-                player.SpawnEnemy(curveRightEnemy,
-                    num, y, -30);
-                player.SpawnEnemy(curveRightEnemy,
-                    num + 1.5f, y, -30);
-            });
+                player.SpawnEnemy(
+                    goStraightEnemy,
+                    baseX + 1.5f,
+                    baseY + 4.5f,
+                    rotationZ
+                );
 
+                player.SpawnEnemy(
+                    goStraightEnemy,
+                    baseX - 1.5f,
+                    baseY + 4.5f,
+                    rotationZ
+                );
 
-            spawnEvent.Add(9800, player =>
-            {
-                const float x = -15f;
-                const float y = 25f;
-                player.SpawnEnemy(curveLeftEnemy,
-                    x - 1.5f, y, 30);
-                player.SpawnEnemy(curveLeftEnemy,
-                    x, y, 30);
-                player.SpawnEnemy(curveLeftEnemy,
-                    x + 1.5f, y, 30);
-            });
-            spawnEvent.Add(9900, player =>
-            {
-                const float x = -15f;
-                const float y = 26.5f;
-                player.SpawnEnemy(curveLeftEnemy,
-                    x - 1.5f, y, 30);
-                player.SpawnEnemy(curveLeftEnemy,
-                    x, y, 30);
-                player.SpawnEnemy(curveLeftEnemy,
-                    x + 1.5f, y, 30);
-            });
-            spawnEvent.Add(10000, player =>
-            {
-                const float x = -15f;
-                const float y = 28f;
-                player.SpawnEnemy(curveLeftEnemy,
-                    x - 1.5f, y, 30);
-                player.SpawnEnemy(curveLeftEnemy,
-                    x, y, 30);
-                player.SpawnEnemy(curveLeftEnemy,
-                    x + 1.5f, y, 30);
+                player.SpawnEnemy(
+                    goStraightEnemy,
+                    baseX,
+                    baseY + 6f,
+                    rotationZ
+                );
             });
         }
     }
