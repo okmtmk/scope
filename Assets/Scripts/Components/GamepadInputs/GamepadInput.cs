ae57340a32964e31dd472cc90604c0d42f931aff
--local <--local>
2019-11-13 21:02:25 +0900

    [fix]

--- Assets/Scripts/Components/GamepadInputs/GamepadInput.cs
+++ Assets/Scripts/Components/GamepadInputs/GamepadInput.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
+﻿using System.Diagnostics;
 using src.positions;
-using System.Collections.Generic;
 using UnityEngine;
-using System.Diagnostics;
 
-public class GamepadInput : MonoBehaviour
+namespace Components.GamepadInputs
 {
-    // Start is called before the first frame update
-    /*void Start()
+    public class GamepadInput : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        /*void Start()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
@@ -20,33 +20,32 @@ public class GamepadInput : MonoBehaviour
         gameObject.transform.position = new Vector3(gameObject.transform.position.x + Input.GetAxis("Horizontal"), gameObject.transform.position.y + Input.GetAxis("Vertical"), 0);
     }*/
 
-    private Stopwatch _stopwatch;
-    private MovablePosition2 _position2;
+        private Stopwatch _stopwatch;
 
-    [SerializeField] private float moveSpeed = 15;
+        [SerializeField] private float moveSpeed = 15;
 
-    private void Start()
-    {
-        _position2 = new MovablePosition2(gameObject.transform.position, 15);
-        _stopwatch = new Stopwatch();
-    }
-
-    private void Update()
-    {
-        var direction = MovingKey.GetMovingDirection();
-
-        if (!(direction.x == 0 && direction.y == 0))
+        private void Start()
         {
-            _position2.Move(direction, moveSpeed, _stopwatch.ElapsedMilliseconds);
-            _stopwatch.Restart();
+            _stopwatch = new Stopwatch();
+        }
 
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x + Input.GetAxis("Horizontal"), gameObject.transform.position.y + Input.GetAxis("Vertical"), 0);
-        }
-        else
+        private void Update()
         {
-            _stopwatch.Stop();
-            _stopwatch.Reset();
+            var direction = MovingKey.GetMovingDirection();
+
+            if (!(direction.x == 0 && direction.y == 0))
+            {
+                _stopwatch.Restart();
+
+                gameObject.transform.position
+                    = new Vector3(gameObject.transform.position.x + Input.GetAxis("Horizontal"),
+                        gameObject.transform.position.y + Input.GetAxis("Vertical"), 0);
+            }
+            else
+            {
+                _stopwatch.Stop();
+                _stopwatch.Reset();
+            }
         }
-        _position2.FitMovableArea();
     }
 }
