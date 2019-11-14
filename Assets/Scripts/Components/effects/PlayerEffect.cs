be46eb57224d33749570e1f66b154a609c9c4ff8
okmtmk <c0118060b4@edu.teu.ac.jp>
2019-11-14 12:19:20 +0900

    [add]適当に被ダメ演出追加

---
+++ Assets/Scripts/Components/effects/PlayerEffect.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Diagnostics;
+using Components.simpleColliders;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Components.effects
+{
+    public class PlayerEffect : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem damagedParticle;
+        [NonSerialized] private Animation _animation;
+        [NonSerialized] private Stopwatch _stopwatch = new Stopwatch();
+        [NonSerialized] private bool _isDamaged;
+
+        private bool IsDamaged
+        {
+            get => _isDamaged;
+            set
+            {
+                _isDamaged = value;
+
+                if (_isDamaged)
+                {
+                    OnDamaged();
+                }
+                else
+                {
+                    OnNonDamaged();
+                }
+            }
+        }
+
+        private void Start()
+        {
+            _animation = GetComponent<Animation>();
+        }
+
+        private void Update()
+        {
+            if (_stopwatch.ElapsedMilliseconds > 5000)
+            {
+                IsDamaged = false;
+            }
+        }
+
+        public void OnEnterCollider(SpriteCollider2D other)
+        {
+            if (other.gameObject.CompareTag("Enemy") && !IsDamaged)
+            {
+                var obj = Instantiate(damagedParticle);
+                obj.transform.position = gameObject.transform.position;
+                IsDamaged = true;
+            }
+        }
+
+        private void OnDamaged()
+        {
+            Debug.Log("ダメージを受けた");
+            _stopwatch.Reset();
+            _stopwatch.Start();
+
+            _animation.enabled = true;
+            _animation.clip.wrapMode = WrapMode.Loop;
+            _animation.Play();
+        }
+
+        private void OnNonDamaged()
+        {
+            Debug.Log("ダメージが消えた");
+            _animation.Stop();
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
+
+            _stopwatch.Stop();
+            _stopwatch.Reset();
+        }
+    }
+}
